633aa3531968e722a2f4c6b73f1cdbc844fe7e7b
Brandon Demonbreun <brando@rockwithu.com>
2024-08-07 12:05:08 -0400

    Changing gallery links to join to gallery
    - Instead of just grabbing the links themselves, I need the gallery data
    in the client

--- app/galleries/controllers/gallery_links.py
+++ app/galleries/controllers/gallery_links.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException
 
 from app.authentication.models import AccessTokenData
 from app.controller import BaseController
-from app.galleries.models import GalleryLink
+from app.galleries.models import Gallery, GalleryLink
 from app.users.models import User
 
 
@@ -17,25 +17,36 @@ class GalleryLinkController(BaseController):
 
     async def get_gallery_links(self):
         query = """SELECT 
+        g.*,
         gl.id as link_id,
         gl.title as link_title,
         gl.link as link_link,
         gl.expiration_date as link_expiration_date,
         gl.view_count as link_view_count,
         gl.date_created as link_date_created,
-
         u.id as user_id,
         u.username,
         u.is_active as user_is_active
-
-        FROM gallery_link AS gl
+        FROM gallery AS g 
+        LEFT JOIN gallery_link AS gl ON gl.gallery_id = g.id
         LEFT JOIN auth_user AS u ON u.id = gl.created_by_id
-        WHERE gl.gallery_id = $1"""
+        WHERE g.id = $1"""
         result: list[Record] = await self.db.select_many(query, self.gallery_id)
         if not result:
             raise HTTPException(status_code=404)
-        output: list[GalleryLink] = []
-
+        base_row: Record = result[0]
+        gallery = Gallery(
+            id=base_row["id"],
+            title=base_row["title"],
+            description=base_row["description"],
+            date_created=base_row["date_created"],
+            created_by=User(
+                id=base_row["user_id"],
+                username=base_row["username"],
+                is_active=base_row["user_is_active"],
+            ),
+        )
+        links: list[GalleryLink] = []
         for row in result:
             view_count = row["link_view_count"] if row["link_view_count"] else 0
             gallery_link = GalleryLink(
@@ -51,8 +62,11 @@ class GalleryLinkController(BaseController):
                     is_active=row["user_is_active"],
                 ),
             )
-            output.append(gallery_link)
-        return output
+            links.append(gallery_link)
+        links.sort(key=lambda x: x.date_created, reverse=True)
+        gallery.links = links
+
+        return gallery
 
     async def gallery_link_update(self, gallery_link_id: int):
         pass
