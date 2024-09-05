ed20b0d76589251b64d041ee3c1c83d9aed981a8
Brandon Demonbreun <brando@rockwithu.com>
2024-09-04 21:16:10 -0400

    Reorganizing tests
    - adding integration and unit test modules

--- tests/integration/test_auth.py
+++ tests/integration/test_auth.py
@@ -3,13 +3,13 @@ from unittest.mock import AsyncMock, patch
 import pytest
 
 from app.authentication.controller import AuthControllerBase
-from app.users.models import User, UserInDB
+from app.users.models import UserInDB
 
-HASHED_PASSWORD = "$2b$12$OjH9jIdb5qpptt/GiWWk0O/hHRmVcopmiciccbPePjBTbhaJLG5Gm"
-PLAIN_PASSWORD = "mypassword"
-WRONG_PASSWORD = "incorrect-password"
+hashed_password = "$2b$12$OjH9jIdb5qpptt/GiWWk0O/hHRmVcopmiciccbPePjBTbhaJLG5Gm"
+plain_password = "mypassword"
+wrong_password = "incorrect-password"
 
-USER = UserInDB(id=1, is_active=True, hashed_password=HASHED_PASSWORD, username="")
+user = UserInDB(id=1, is_active=True, hashed_password=hashed_password, username="")
 
 
 @pytest.fixture
@@ -32,14 +32,14 @@ class TestLogin:
     def test_login_active_user(
         self, client, mock_get_user_in_db, mock_create_token_pair
     ):
-        mock_get_user_in_db.return_value = USER
-        output_user = USER.model_dump(exclude={"hashed_password"})
+        mock_get_user_in_db.return_value = user
+        output_user = user.model_dump(exclude={"hashed_password"})
         mock_create_token_pair.return_value = {
             "access_token": "<AccessToken>",
             "refresh_token": "<RefreshToken>",
             "user": output_user,
         }
-        payload = {"username": "", "password": PLAIN_PASSWORD}
+        payload = {"username": "", "password": plain_password}
         response = client.post("/api/authentication/login", json=payload)
         assert response.status_code == 200
         assert response.json() == {
@@ -49,32 +49,24 @@ class TestLogin:
         }
 
     def test_login_incorrect_password(self, client, mock_get_user_in_db):
-        mock_get_user_in_db.return_value = USER
-        payload = {"username": "", "password": WRONG_PASSWORD}
+        mock_get_user_in_db.return_value = user
+        payload = {"username": "", "password": wrong_password}
         response = client.post("/api/authentication/login", json=payload)
         assert response.status_code == 400
 
     def test_login_inactive_user(self, client, mock_get_user_in_db):
-        USER.is_active = False
-        mock_get_user_in_db.return_value = USER
-        payload = {"username": "", "password": PLAIN_PASSWORD}
+        user.is_active = False
+        mock_get_user_in_db.return_value = user
+        payload = {"username": "", "password": plain_password}
         response = client.post("/api/authentication/login", json=payload)
         assert response.status_code == 401
 
 
 class TestRefreshToken:
-    @staticmethod
-    def get_access_token(client):
-        payload = {"username": "", "password": PLAIN_PASSWORD}
-        response = client.post("/api/authentication/login", json=payload)
-        data = response.json()
-        refresh_token = data["refresh_token"]
-        return refresh_token
-
     def test_refresh_token(self, client, mock_get_user_in_db):
-        USER.is_active = True
-        mock_get_user_in_db.return_value = USER
-        refresh_token = self.get_access_token(client)
+        user.is_active = True
+        mock_get_user_in_db.return_value = user
+        refresh_token = self._get_access_token(client)
         headers = {"Authorization": "Bearer " + refresh_token}
         response = client.post("/api/authentication/refresh-tokens", headers=headers)
         assert response.status_code == 200
@@ -85,6 +77,14 @@ class TestRefreshToken:
         assert response.status_code == 401
         response = client.post("/api/authentication/refresh-tokens")
         assert response.status_code == 401
+
+    @staticmethod
+    def _get_access_token(client):
+        payload = {"username": "", "password": plain_password}
+        response = client.post("/api/authentication/login", json=payload)
+        data = response.json()
+        refresh_token = data["refresh_token"]
+        return refresh_token
 
 
 class TestPermissions:
---
+++ tests/integration/test_items_bucket.py
@@ -0,0 +1,27 @@
+from io import BytesIO
+from unittest.mock import AsyncMock, patch
+
+import pytest
+
+from app.items.bucket.controllers.item_upload import BatchUploadController
+
+
+@pytest.fixture
+def mock_s3_api_controller():
+    with patch.object(
+        BatchUploadController, "s3_batch_upload", new_callable=AsyncMock
+    ) as mock_s3_api_controller:
+        yield mock_s3_api_controller
+
+
+class TestItemBucket:
+    def test_item_batch_upload(self, client, mock_s3_api_controller):
+        mock_s3_api_controller.return_value = {"new_keys": ["key1", "key2"]}
+        files = [
+            ("files", ("file1.txt", BytesIO(b"file1 content"), "text/plain")),
+            ("files", ("file2.txt", BytesIO(b"file2 content"), "text/plain")),
+        ]
+        data = {"source_id": 123, "encryption_key": "my-secret-key"}
+        response = client.post("/api/items/bucket", params=data, files=files)
+        assert response.status_code == 200
+        assert response.json() == {"new_keys": ["key1", "key2"]}
