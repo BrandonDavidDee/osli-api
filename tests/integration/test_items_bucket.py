c5189d82ed195491c1b67b116dd4ac2c840f5359
Brandon Demonbreun <brando@rockwithu.com>
2024-09-05 17:14:41 -0400

    Adding another test
    - testing bucket item search with results

--- tests/integration/test_items_bucket.py
+++ tests/integration/test_items_bucket.py
@@ -1,5 +1,6 @@
 from io import BytesIO
-from unittest.mock import AsyncMock, patch
+from datetime import datetime
+from unittest.mock import AsyncMock, patch, MagicMock
 
 import pytest
 
@@ -21,7 +22,7 @@ def mock_database_select_many():
         yield mock_method
 
 
-class TestItemBucket:
+class TestItemBucketUpload:
     def test_item_batch_upload(self, client, mock_s3_api_controller):
         mock_s3_api_controller.return_value = {"new_keys": ["key1", "key2"]}
         files = [
@@ -33,14 +34,45 @@ class TestItemBucket:
         assert response.status_code == 200
         assert response.json() == {"new_keys": ["key1", "key2"]}
 
-    def test_item_search(self, client):
+
+class TestItemBucketSearch:
+    url = "/api/items/bucket/search?source_id=1"
+
+    def test_search_no_tags(self, client):
+        sample_payload = {"tag_ids": []}
+        mock_db_response = [
+            {
+                "total_count": 1075,
+                "id": 1111,
+                "source_bucket_id": 1,
+                "title": "Some Title",
+                "mime_type": "image/jpeg",
+                "file_path": "images/some_file.jpg",
+                "file_size": 123456789,
+                "notes": None,
+                "date_created": "2024-08-08T19:17:38.419580+00:00",
+                "created_by_id": 1,
+                "source_title": "Source Title",
+                "bucket_name": "bucket-name",
+                "media_prefix": "",
+                "grid_view": True,
+            },
+        ]
+        with patch(
+            "app.db.Database.select_many", return_value=mock_db_response
+        ) as mock_select_many:
+            response = client.post(self.url, json=sample_payload)
+            assert response.status_code == 200
+            mock_select_many.assert_called_once()
+            data = response.json()
+            assert "source" in data
+            assert "items" in data
+            assert len(data["items"]) == len(mock_db_response)
+
+    def test_search_no_tags_no_results(self, client):
         sample_payload = {"tag_ids": []}
         mock_db_response = []
-        mock_source_detail_response = {
-            "id": 1,
-            "source_type": "Mocked Source",
-            "title": "Title",
-        }
+        mock_source_detail_response = MagicMock()
         with patch(
             "app.db.Database.select_many", return_value=mock_db_response
         ) as mock_select_many, patch.object(
@@ -48,15 +80,10 @@ class TestItemBucket:
             "source_detail",
             return_value=mock_source_detail_response,
         ) as mock_source_detail:
-            source_id = 1
-            response = client.post(
-                f"/api/items/bucket/search?source_id={source_id}", json=sample_payload
-            )
+            response = client.post(self.url, json=sample_payload)
             assert response.status_code == 200
-
             mock_select_many.assert_called_once()
-            mock_source_detail.assert_called_once()
-
+            mock_source_detail.assert_called_once()  # is called when there are no results
             data = response.json()
             assert "source" in data
             assert "items" in data
